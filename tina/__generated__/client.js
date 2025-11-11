084081e94380658907aa43995d4b805df4b914ed
yanililas <yani.liu@essec.edu>
2025-11-11 18:40:18 +0100

    Complete TinaCMS setup - add generated schema and configuration files

--- tina/__generated__/client.js
+++ tina/__generated__/client.js
@@ -1,2 +1,4 @@
-export default ()=>({})
-export const client = ()=>({})
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ url: "http://localhost:4001/graphql", token: "undefined", queries });
+export default client;
---
+++ tina/__generated__/types.js
@@ -0,0 +1,117 @@
+export function gql(strings, ...args) {
+  let str = "";
+  strings.forEach((string, i) => {
+    str += string + (args[i] || "");
+  });
+  return str;
+}
+export const ArticlePartsFragmentDoc = gql`
+    fragment ArticleParts on Article {
+  __typename
+  title
+  category
+  publishDate
+  readTime
+  excerpt
+  featuredImage
+  author
+  body
+  published
+}
+    `;
+export const ArticleDocument = gql`
+    query article($relativePath: String!) {
+  article(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...ArticleParts
+  }
+}
+    ${ArticlePartsFragmentDoc}`;
+export const ArticleConnectionDocument = gql`
+    query articleConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: ArticleFilter) {
+  articleConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...ArticleParts
+      }
+    }
+  }
+}
+    ${ArticlePartsFragmentDoc}`;
+export function getSdk(requester) {
+  return {
+    article(variables, options) {
+      return requester(ArticleDocument, variables, options);
+    },
+    articleConnection(variables, options) {
+      return requester(ArticleConnectionDocument, variables, options);
+    }
+  };
+}
+import { createClient } from "tinacms/dist/client";
+const generateRequester = (client) => {
+  const requester = async (doc, vars, options) => {
+    let url = client.apiUrl;
+    if (options?.branch) {
+      const index = client.apiUrl.lastIndexOf("/");
+      url = client.apiUrl.substring(0, index + 1) + options.branch;
+    }
+    const data = await client.request({
+      query: doc,
+      variables: vars,
+      url
+    }, options);
+    return { data: data?.data, errors: data?.errors, query: doc, variables: vars || {} };
+  };
+  return requester;
+};
+export const ExperimentalGetTinaClient = () => getSdk(
+  generateRequester(
+    createClient({
+      url: "http://localhost:4001/graphql",
+      queries
+    })
+  )
+);
+export const queries = (client) => {
+  const requester = generateRequester(client);
+  return getSdk(requester);
+};
